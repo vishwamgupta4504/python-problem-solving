2f77f569cf18ec00adae0eecafb87ee189d6e2ff
vishwam gupta <vishwamgupta02@gmail.com>
2026-09-18 14:35:14 +0530

    remove list duplicates

--- practice-questions.py
+++ practice-questions.py
@@ -66,11 +66,18 @@
 
 #  string
 
-text="programming"
-count=0
+# text="programming"
+# count=0
 
-for i in text:
-    if "g" in i:
-        count+=1
+# for i in text:
+#     if "g" in i:
+#         count+=1
 
-print(count)
+# print(count)
+
+#  remove duplicates
+numbers=[10,20,10,30,20,40,10]
+numbers=list(dict.fromkeys(numbers))
+print(numbers)
+
+
