b45de802360fe6dbd635b210b2f1cf7e625eead5
vishwam gupta <vishwamgupta02@gmail.com>
2026-09-18 12:38:33 +0530

    students marks analyzer

--- practice-questions.py
+++ practice-questions.py
@@ -2,3 +2,16 @@
 
 text="python"
 print(text[::-1])
+
+# student marks Analzer
+
+students=[]
+
+while True:
+    def Analyzer():
+        student_Name=input("Enter student name: ")
+        student_Marks= int(input("Enter Student marks: "))
+
+        students.append(student_Name,student_Marks)
+        print(students)
+
